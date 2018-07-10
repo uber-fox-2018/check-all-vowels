46004d779002d266f7512faa07a46d27f73dbc63
Andre Sudi Yudatama <andresudi@DDCBU-elee.localdomain>
2018-07-10 15:04:01 +0700

    check all vowels -andre sudi

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
 // your code here
+function checkAllVowels(str) {
+    let vocals = 'aiueo'
+    let output = ''
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vocals.length; j++) {
+            if (str[i].toLowerCase() == vocals[j]) {
+                output += str[i]
+            }
+        }
+    }
+
+    if (output.length == str.length) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(checkAllVowels('aaaoooeeuuuii'))
+console.log(checkAllVowels('arrraubbuuii'))
+console.log(checkAllVowels('DnsOp'))
