28ab73a6dcd59be8f1180100a9d3a34665a86c4d
Your Name <fahrisyany@gmail.com>
2018-07-10 17:23:14 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,35 @@
-// your code here
+
+function checkVowels(str) {
+
+    var vowels = 'aiueo'
+    var vowelCounter = 0
+    var string=''
+
+    for (j = 0; j < str.length; j++) {
+        if(str[j]!==' '){
+
+            string+=str[j]
+        }
+    }
+
+    for (i = 0; i < vowels.length; i++) {
+        for (j = 0; j < string.length; j++) {
+            if (vowels[i] === string[j]) {
+                vowelCounter++
+            }
+            if (vowelCounter === string.length) {
+                return true
+            }
+        }
+    }
+    return false
+
+}
+
+
+
+
+console.log(checkVowels('aiu ueeeo'));
+
+console.log(checkVowels('aiieeoob'));
+
