1b579ad1553d4fcd4a53c73e8a65c38bd1b400e1
Odelia <odeliasusanto@gmail.com>
2018-07-10 11:54:19 +0700

    check vowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,26 @@
 // your code here
+function checkVowels(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let isVowels = false;
+
+    for (let i = 0; i < str.length; i++) {
+        let vowelCount = 0;
+        for (let j = 0; j < vowels.length; j++) {
+            if (str[i] === vowels[j]) {
+                vowelCount++;
+            }
+        }
+        if (vowelCount === 0) {
+            return false;
+        } else {
+            isVowels = true;
+        }
+    }
+    return isVowels;
+}
+
+console.log(checkVowels('aeiou')); //true
+console.log(checkVowels('abior')); //false
+console.log(checkVowels('OUIAE')); //true
+console.log(checkVowels('abcde')); //false
+console.log(checkVowels('OUOUAI')); //true
