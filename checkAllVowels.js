36be392b36d7090addb42ac71945bd9935be2156
Susan Nio <susan.wen88@gmail.com>
2018-07-10 13:11:25 +0700

    Adding checkAllVowels.js

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,11 @@
-// your code here
+function checkVowel(str){
+    let arr = str.toLowerCase().split("");
+    return arr.every(function(word){
+        return "aiueo".includes(word);
+    });
+}
+console.log(checkVowel("apple"));//false
+console.log(checkVowel("aaaaaaa"));//true
+console.log(checkVowel("haihahaa"));//false
+console.log(checkVowel("AAoooOOOEEEee"));//true
+
