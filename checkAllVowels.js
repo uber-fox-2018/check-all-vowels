0f3eb18d196ac36de10c2e7d6d5bddad961855ac
sbachr <sbachr0501@gmail.com>
2018-07-10 14:58:48 +0700

    fix check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
-// your code here
+function checkAllVowels(str) {
+    let vocal = 'aiueo'
+    let strWithoutSpace = []
+    let strCount = 0
+
+    for (let a = 0; a < str.length; a++) {
+        if (str[a] != ' ') strWithoutSpace.push(str[a])
+        for (let b = 0; b < vocal.length; b++) {
+            if (str[a] == vocal[b]) strCount++
+        }
+    }
+    return (strWithoutSpace.length == strCount ? true : false)
+}
+
+console.log(checkAllVowels('aaaaa'));
+console.log(checkAllVowels('a aa aa'));
+console.log(checkAllVowels('a!a'));
+console.log(checkAllVowels('aaiiuu'))
