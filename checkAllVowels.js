78c9ecba7610355f63e8c27d276b63b754d0d628
wisnugautama <ngurahwisnugautama@gmail.com>
2018-07-10 11:36:07 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,24 @@
 // your code here
+function checkVowel(string) {
+    let vowels = ['a','i','u','e','o']
+    let checker = 0;
+
+    for (let i = 0; i < string.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if (string[i] === vowels[j] ) {
+                checker++;
+            }
+        }
+
+        if (checker === 0) {
+           return false 
+        }
+
+    }
+    return true;
+}
+
+console.log(checkVowel('test'));
+console.log(checkVowel('kemana'));
+console.log(checkVowel('aaaaaaaiiiiiuuuu'));
+
