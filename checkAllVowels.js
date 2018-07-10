85a905c7bc6895af76e1dc9976cc4dd77ef7694a
Fajar Tri Cahyono <test@MZamronnsiPhone.localdomain>
2018-07-10 10:37:07 +0700

    done checkAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
 // your code here
+function checkAllVowels(str) {
+    let toLowCase = str.toLowerCase()
+    let vocals = 'aiueo'
+    let containStr = ''
+    for (let i = 0; i < toLowCase.length; i++) {
+        for(let j = 0; j < vocals.length; j++) {
+            if (toLowCase[i] === vocals[j]) {
+                containStr += toLowCase[i]
+            }
+        }
+    }
+    if (containStr.length === toLowCase.length) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(checkAllVowels('AiAu'));
