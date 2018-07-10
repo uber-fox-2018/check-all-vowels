d3e5200e7bd4996cddb5abeb95ade49d9e24d14c
gandryeanb <gstandrianb@gmail.com>
2018-07-10 17:24:19 +0700

    adding js file

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
-// your code here
+function checkVowels(str) {
+    var vowels = 'aiueo '
+
+    if (str.length === 0) {
+        return true
+    } else {
+        var trigger = 0
+        for (var i = 0; i < vowels.length; i++) {
+            if (str[0].toLowerCase() === vowels[i]) {
+                trigger++
+            }
+        }
+        if(trigger === 0) {
+            return false
+        }
+        return checkVowels(str.slice(1))
+    }
+}
+
+console.log(checkVowels("ai ueo"))
