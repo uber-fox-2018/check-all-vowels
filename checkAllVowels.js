61b0af41ad3882c6820cd37df6cf28145d713429
rizariza69 <riza.riza69@gmail.com>
2018-07-10 10:33:19 +0700

    check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
 // your code here
+
+function checkVowel(string){
+    let vocal = 'aiueo'
+
+    for(let i = 0 ; i < vocal.length ; i++){
+        for(let j = 0 ; j < string.length ; j++){
+
+            if (string[j] === vocal[i]){
+                return true
+            }
+        }
+    }
+    return false
+}
+console.log(checkVowel('brmmmm'))
+console.log(checkVowel('aueioae'))
+console.log(checkVowel('qwrtuipm'))
+console.log(checkVowel('qrqtqyqtqyrytyhgnvm'))
