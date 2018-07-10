f9113a0d12b1524c4a296a5f53207127e942e834
Grayfield <grayfieldmm@outlook.com>
2018-07-10 13:13:13 +0700

    Finished check all vowels challenge

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
 // your code here
+function checkAllVowels(str) {
+    let vowels = ['a', 'i', 'u', 'e', 'o'];
+
+    if(str.length === 0) return false;
+
+    for(let i = 0; i < str.length; i++)
+        if(str[i] === ' ') continue
+        else if(!vowels.includes(str[i])) return false;
+    return true;
+}
+
+console.log('abcde', checkAllVowels('abcde'));
+console.log('oeuia', checkAllVowels('oeuia'));
+console.log('', checkAllVowels(''));
+console.log('i oe u', checkAllVowels('i oe u'));
