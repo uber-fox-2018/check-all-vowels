0db994fbb7bb4bad88f8df179f396c7ca47d2960
vinesto <oqvinesto@gmail.com>
2018-07-10 13:50:39 +0700

    First commit

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
 // your code here
+function checkVowels(words){
+    let vowel = "aiueoAIUEO"
+    let counter = 0;
+    for(let i = 0; i<vowel.length; i++){
+        for(let j = 0; j<words.length; j++){
+            if(vowel[i] === words[j]){
+                counter++
+            }
+        }
+    }
+    if(counter === words.length){
+        return true
+    }else{
+        return false
+    }
+}
