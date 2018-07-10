067ab8e21be60063858109b935908ee8b659bcf7
minzarddillah <minzard.dillah@gmail.com>
2018-07-10 19:12:21 +0700

    check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,24 @@
-// your code here
+// check all vowels 
+// learning competencies 
+// - mealakian problem solving di javascript
+
+// realease
+// release 0: check vowel
+// buat sebuah funtion yang mengecek apakah suatu string terdiri dari huruf vokal semua atau tidak tanpa menggunakan regular expression(regex). return true bila ditemukan dan false bila tidak
+
+
+function checkAllVowels(str){
+    var vokal = 'aiueo'
+
+    for(var i = 0 ; i < str.length ; i++){
+        for(var j = 0 ; j < vokal.length ; j++){
+            if(str[i] === vokal[j]){    
+                return true
+            }
+        }
+    }
+    return false
+}
+console.log(checkAllVowels('aaafdfd')) // false
+
+// jika semua str adalah vokal maka return true
