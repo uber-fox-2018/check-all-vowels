ef6953bba4d833cda936caf3e9e4535700ed1c75
windra <pwindrap18@gmail.com>
2018-07-10 16:16:56 +0700

    add checkAllVowels.js

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
-// your code here
+function checkVowels(str){
+    let notVowels = 'bcdfghjklmnpqrstvwxyz'
+    for (let i = 0; i < str.length; i++){
+        for (let j = 0; j < notVowels.length; j++){
+            if (str[i]===notVowels[j]){
+                return false
+            }
+        }
+    }
+    return true
+}
+
+console.log(checkVowels('aeo'))
