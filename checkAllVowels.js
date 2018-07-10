84c8ac94a531c30609fa02272b925f26ec5e6e9a
HelmiYogantara <helmiyogantara@gmail.com>
2018-07-10 13:59:41 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,26 @@
 // your code here
+function cekVowels(string) {
+    var vowels = 'AIUEO'
+    var count = 0
+    for(var i = 0; i < string.length;i++){
+        for(var j = 0; j < vowels.length;j++){
+            if(string[i].toUpperCase() === vowels[j]){
+                // return true
+                count++
+            }
+        }
+    }
+    
+    if(count === string.length){
+        return true
+    }
+    return false
+}
+
+console.log(cekVowels('aku'));
+console.log(cekVowels('bbbbb'));
+console.log(cekVowels('AABVIDA'));
+console.log(cekVowels('aaaaa'));
+console.log(cekVowels('AAA'));
+
+
