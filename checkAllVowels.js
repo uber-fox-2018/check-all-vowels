84993db8e5c559b803febaae956adb1db692f380
riadji.github.io <riadji.ramadhan@gmail.com>
2018-07-10 14:56:53 +0700

    Add Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
-// your code here
+function check(vowels) {
+  let v = 'aiueo';
+  let str = vowels.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < v.length; j++) {
+      if (str[i] === v[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+console.log(check('Absjn'));
+console.log(check('ghjywt'));
